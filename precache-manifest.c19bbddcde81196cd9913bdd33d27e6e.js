5d6d6fcedd7dc8434bb80bdf44a994978a230b2f
zoltangy <zoltangy@whatever.com>
2020-03-18 16:20:21 +0100

    Updates

---
+++ precache-manifest.c19bbddcde81196cd9913bdd33d27e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83da0d631df36bd6e2cf91b466487f18",
+    "url": "/web-calculator/index.html"
+  },
+  {
+    "revision": "c419ba9d8222fab32d3c",
+    "url": "/web-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ec8a8199d13578d5de9",
+    "url": "/web-calculator/static/js/2.c4a13faf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/web-calculator/static/js/2.c4a13faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c419ba9d8222fab32d3c",
+    "url": "/web-calculator/static/js/main.dd6cc3e9.chunk.js"
+  },
+  {
+    "revision": "17839ba99f01a5464d56",
+    "url": "/web-calculator/static/js/runtime-main.3483dad7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
