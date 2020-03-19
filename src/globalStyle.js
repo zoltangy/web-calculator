146b7aff1e349946cbe7a87234f41f1143d8759e
zoltangy <zoltangy@whatever.com>
2020-03-19 14:55:36 +0100

    New base

---
+++ src/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+    background-color: pink;
+  }
+`;
+
+export default GlobalStyle;
